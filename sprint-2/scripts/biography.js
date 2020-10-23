07616d59e329ecdb765dde36c41689973899a008
David <62397679+MacDavid1989@users.noreply.github.com>
2020-10-23 15:07:50 -0400

    Create variables for all elements in div comments__cards

--- sprint-2/scripts/biography.js
+++ sprint-2/scripts/biography.js
@@ -1,3 +1,12 @@
 const comments = document.querySelector('.comments__cards');
+const card = document.querySelectorAll('.card');
+const cardImage = document.querySelectorAll('.card__image');
+const cardBody = document.querySelectorAll('.card__body');
+const cardHeader = document.querySelectorAll('.card__heading');
+const cardTitle = document.querySelectorAll('.card__text-title');
+const cardDate = document.querySelectorAll('.card__label');
+const cardComment = document.querySelectorAll('.card__text');
 
 console.log(comments);
+
+// comments.innerHTML = ''; // removes content from .comments__cards div
