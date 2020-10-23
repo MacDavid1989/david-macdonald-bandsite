2e260a35b8e05653c09912394bed5b8196d77149
David <62397679+MacDavid1989@users.noreply.github.com>
2020-10-23 15:57:03 -0400

    Create function that renders new comments to page.

--- sprint-2/scripts/biography.js
+++ sprint-2/scripts/biography.js
@@ -1,11 +1,11 @@
 const comments = document.querySelector('.comments__cards');
-const card = document.querySelectorAll('.card');
-const cardImage = document.querySelectorAll('.card__image');
-const cardBody = document.querySelectorAll('.card__body');
-const cardHeader = document.querySelectorAll('.card__heading');
-const cardTitle = document.querySelectorAll('.card__text-title');
-const cardDate = document.querySelectorAll('.card__label');
-const cardComment = document.querySelectorAll('.card__text');
+// const card = document.querySelectorAll('.card');
+// const cardImage = document.querySelectorAll('.card__image');
+// const cardBody = document.querySelectorAll('.card__body');
+// const cardHeader = document.querySelectorAll('.card__heading');
+// const cardTitle = document.querySelectorAll('.card__text-title');
+// const cardDate = document.querySelectorAll('.card__label');
+// const cardComment = document.querySelectorAll('.card__text');
 const button = document.querySelector('.button');
 const form = document.querySelector('.form')
 
@@ -18,3 +18,47 @@ function formHandler(e) {
     console.log(e.target.userName.value);
     console.log(e.target.userComment.value);
 };
+
+function renderComments (comment) {
+    // create Card
+    const cardEl = document.createElement('div');
+    cardEl.classList.add('card');
+    comments.prepend(cardEl);
+
+    // create Image
+    const imageEl = document.createElement('div');
+    imageEl.classList.add('card__image');
+    cardEl.appendChild(imageEl);
+
+    // create Card Body
+    const bodyEl = document.createElement('div');
+    bodyEl.classList.add('card__body');
+    cardEl.appendChild(bodyEl);
+
+    // create Card Header
+    const headerEl = document.createElement('div');
+    headerEl.classList.add('card__heading');
+    bodyEl.appendChild(headerEl);
+
+    // create Card Text
+    const paragraphEl = document.createElement('p');
+    paragraphEl.classList.add('card__text');
+    paragraphEl.innerText = 'hello'; // test for paragraph
+    bodyEl.appendChild(paragraphEl);
+
+    // create Card Name
+    const nameEl = document.createElement('h2');
+    nameEl.classList.add('card__text-title');
+    nameEl.innerText = "is it me"; // test for name
+    headerEl.appendChild(nameEl);
+
+    // create Card Date
+    const dateEl = document.createElement('h5');
+    dateEl.classList.add('card__label');
+    dateEl.innerText = 'you\'re looking for' // test for date
+    headerEl.appendChild(dateEl);
+
+    return cardEl;
+}
+
+console.log(renderComments());
