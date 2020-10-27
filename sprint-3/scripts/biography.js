bc4880c3bc5f6aae627f592c3b275ffaa98af957
David <62397679+MacDavid1989@users.noreply.github.com>
2020-10-26 22:37:57 -0400

    Create delete comment function which when invoked passes parameters to axios.delete to remove a comment. Add button handler function inside render comments to select buttons which delete comments. Need to fix data passed into delete comment function.

--- sprint-3/scripts/biography.js
+++ sprint-3/scripts/biography.js
@@ -1,26 +1,90 @@
-// AXIOS
-
-const getApiKey = () => {
-	axios.get('https://project-1-api.herokuapp.com/register')
-	.then(response => {
-		const apiKey = response.data.api_key;
-		axios.get('https://project-1-api.herokuapp.com/comments?api_key='+apiKey)
-			.then(response => {
-        		return response.data;
-			})	
-			.then(comments => {
-				comments.forEach(object => displayComments(object));
-				// event listener for when the form button is pressed and the info is submitted
-				form.addEventListener('submit', formHandler);
-			})
-	})
-}
-
 // constant variable to select div with class "comments__cards"
 const comments = document.querySelector('.comments__cards'); 
 
 // constant variable to select form element with class "form"
 const form = document.querySelector('.form'); 
+
+const deleteComment = (el, id) => {
+	axios.delete('https://project-1-api.herokuapp.com/comments/#{id}?api_key=' + apiKey)
+	.then(response => {
+		console.log(`DELETE: user is removed`, id);
+		// remove elem from DOM
+		el.remove();
+	})
+	.catch(error => console.error(error));
+};
+
+
+// 
+const header = {
+	'Content-Type': 'application/json'
+};
+
+// anonymous function to retrieve an api key and store it in a variable 
+const getApiKey = () => {
+	axios.get('https://project-1-api.herokuapp.com/register')
+	.then(response => {
+		return response.data.api_key;		
+	})
+};
+
+// contains the returned value of the invoked anonymous promise function
+const apiKey = getApiKey();
+
+// retrieves the comment objects array and then renders the objects to the browser
+const renderComments = () => {
+	axios.get('https://project-1-api.herokuapp.com/comments?api_key=' + apiKey)
+	.then(response => {
+		console.log(response.data);
+		return response.data;
+	})	
+	.then(comments => {
+		comments.forEach(object => displayComments(object));
+		const button = document.querySelector('.card');
+		console.log(button);
+		button.onclick = (e) => {
+			e.stopPropagation();
+			deleteComment(e, e.target.id);
+		}
+	});
+};
+
+// renders comments when page loads
+renderComments();
+
+// event listener for when the form button is pressed and the info is submitted
+form.addEventListener('submit', formHandler);
+
+// function that deals with the form submission generated event object
+function formHandler(e) {
+	// declares new comment object
+	const newComment = {}; 
+
+    // prevents page reload upon submission
+    e.preventDefault(); 
+
+    // creates name object key and sets it to the given value of the input element
+    newComment.name = e.target.userName.value; 
+
+    // creates comment object key and sets it to the given value of the textarea element
+    newComment.comment = e.target.userComment.value; 
+
+    // clear input field
+    document.getElementById('name').value = '';
+
+    // clear comment textarea 
+    document.getElementById('comment').value = ''; 
+
+    // clear comments list
+	comments.innerHTML = '';
+
+	// post new comment object to server
+	axios.post('https://project-1-api.herokuapp.com/comments?api_key=' + apiKey, newComment, header)
+	.then( (response) => {
+		console.log(response);
+	renderComments();
+	});
+};
 
 // function that creates comment section cards
 function displayComments (comment) {
@@ -28,6 +92,7 @@ function displayComments (comment) {
 	// create Card
 	const cardEl = document.createElement('div');
 	cardEl.classList.add('card');
+	cardEl.setAttribute('id', comment.id);
 	comments.prepend(cardEl);
 
 	// create Card Image
@@ -62,44 +127,13 @@ function displayComments (comment) {
 	dateEl.classList.add('card__label');
 	dateEl.innerText = convertTime(comment.timestamp);
 	headerEl.appendChild(dateEl);
-};
 
-// declares new comment object
-const newComment = {}; 
-
-// function that deals with the form submission generated event object
-function formHandler(e) {
-
-    // prevents page reload upon submission
-    e.preventDefault(); 
-
-    // creates name object key and sets it to the given value of the input element
-    newComment.name = e.target.userName.value; 
-
-    // creates comment object key and sets it to the given value of the textarea element
-    newComment.comment = e.target.userComment.value; 
-
-    // clear input field
-    document.getElementById('name').value = '';
-
-    // clear comment textarea 
-    document.getElementById('comment').value = ''; 
-
-    // clear comments list
-	comments.innerHTML = '';
-
-	axios.post('https://project-1-api.herokuapp.com/comments?api_key='+apiKey, newComment,)
-	.then(response => {
-		console.log(response);	
-		axios.get('https://project-1-api.herokuapp.com/comments?api_key='+apiKey)
-		.then(response => {
-			console.log(response.data);
-			return response.data;
-		})
-		.then(comments => {
-			comments.forEach(object => displayComments(object));
-		})
-	})
+	 // create Card Button
+	 const buttonEl = document.createElement('button');
+	 buttonEl.classList.add('button');
+	 buttonEl.setAttribute('type', 'button');
+	 buttonEl.innerText = 'DELETE';
+	 cardEl.appendChild(buttonEl);
 };
 
 // returns a string stating how long since the comment was first submitted
@@ -134,6 +168,3 @@ function convertTime(timestamp) {
   return Math.floor(time) + " seconds ago";
   }
 };
-
-const apiKey = getApiKey();
-console.log(apiKey);
