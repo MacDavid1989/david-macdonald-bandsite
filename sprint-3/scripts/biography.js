39faa690c0f2f7db132b5041985fc1c9a12dfe50
David <62397679+MacDavid1989@users.noreply.github.com>
2020-10-26 21:40:26 -0400

    Remove const object variable from formHandler and place in global scope. Resolve comment object generation issue. Place axios.post in formHandler with chained .get to re-render comments.

--- sprint-3/scripts/biography.js
+++ sprint-3/scripts/biography.js
@@ -1,27 +1,26 @@
 // AXIOS
 
-axios.get('https://project-1-api.herokuapp.com/register')
-.then(response => {
-    const apiKey = response.data.api_key;
-    axios.get('https://project-1-api.herokuapp.com/comments?api_key='+apiKey).then(response => {
-        return response.data;
-    }).then(comments => {
-		comments.forEach(object => displayComments(object));
-		// event listener for when the form button is pressed and the info is submitted
-		form.addEventListener('submit', formHandler);
+const getApiKey = () => {
+	axios.get('https://project-1-api.herokuapp.com/register')
+	.then(response => {
+		const apiKey = response.data.api_key;
+		axios.get('https://project-1-api.herokuapp.com/comments?api_key='+apiKey)
+			.then(response => {
+        		return response.data;
+			})	
+			.then(comments => {
+				comments.forEach(object => displayComments(object));
+				// event listener for when the form button is pressed and the info is submitted
+				form.addEventListener('submit', formHandler);
+			})
 	})
-})
-.catch(()=> console.log('key not found'));
-
+}
 
 // constant variable to select div with class "comments__cards"
 const comments = document.querySelector('.comments__cards'); 
 
 // constant variable to select form element with class "form"
 const form = document.querySelector('.form'); 
-
-// declares an empty array that will store the original values of the time in milliseconds since the document was loaded until each form is submitted; before page refresh
-const originalTimeValues = [];
 
 // function that creates comment section cards
 function displayComments (comment) {
@@ -63,16 +62,16 @@ function displayComments (comment) {
 	dateEl.classList.add('card__label');
 	dateEl.innerText = convertTime(comment.timestamp);
 	headerEl.appendChild(dateEl);
-  }
+};
+
+// declares new comment object
+const newComment = {}; 
 
 // function that deals with the form submission generated event object
 function formHandler(e) {
 
     // prevents page reload upon submission
     e.preventDefault(); 
-
-    // declares new comment object
-    const newComment = {}; 
 
     // creates name object key and sets it to the given value of the input element
     newComment.name = e.target.userName.value; 
@@ -89,27 +88,19 @@ function formHandler(e) {
     // clear comments list
 	comments.innerHTML = '';
 
-	axios.get('https://project-1-api.herokuapp.com/register')
+	axios.post('https://project-1-api.herokuapp.com/comments?api_key='+apiKey, newComment,)
 	.then(response => {
-		const apiKey = response.data.api_key;
-		console.log(apiKey);
-		axios.post('https://project-1-api.herokuapp.com/comments?api_key='+apiKey, newComment)
+		console.log(response);	
+		axios.get('https://project-1-api.herokuapp.com/comments?api_key='+apiKey)
 		.then(response => {
-			console.log(response);	
-			axios.get('https://project-1-api.herokuapp.com/comments?api_key='+apiKey)
-			.then(response => {
-				console.log(response.data);
-        		return response.data;
-			})
-			.then(comments => {
-				comments.forEach(object => displayComments(object));
-			})
+			console.log(response.data);
+			return response.data;
+		})
+		.then(comments => {
+			comments.forEach(object => displayComments(object));
 		})
 	})
-	.catch( ()=> console.log('key not found'));
-
 };
-
 
 // returns a string stating how long since the comment was first submitted
 function convertTime(timestamp) {
@@ -142,4 +133,7 @@ function convertTime(timestamp) {
     // returns string with time rounded to the nearest second
   return Math.floor(time) + " seconds ago";
   }
-}
+};
+
+const apiKey = getApiKey();
+console.log(apiKey);
